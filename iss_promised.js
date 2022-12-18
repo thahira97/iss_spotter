97da978864f79f232e1b7de096408c197fb2b58e
thahira97 <thahirasheerin3@gmail.com>
2022-12-18 15:36:55 -0500

    Refactored code using promise

---
+++ iss_promised.js
@@ -0,0 +1,26 @@
+const request = require("request-promise-native");
+const fetchMyIP = function () {
+  return request("https://api.ipify.org?format=json");
+};
+const fetchCoordsByIP = function (body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+}
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+          .then(fetchCoordsByIP)
+          .then(fetchISSFlyOverTimes)
+          .then((data) => {
+            const {response} = JSON.parse(data);
+            // console.log(data)
+            // console.log(response)
+            return response; // Doesn't work except for response key word??
+          })
+        }
+          module.exports = { nextISSTimesForMyLocation }
+
