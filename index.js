7cd74931aac602c66579f2f927b6d6a3ab8fa9fe
thahira97 <thahirasheerin3@gmail.com>
2022-12-17 23:19:25 -0500

    Callback Hell

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { fetchMyIP } = require("./iss");
 const { fetchCoordsByIp } = require("./iss");
 const { fetchISSFlyOverTimes } = require("./iss");
-
+const { nextISSTimesForMyLocation } = require('./iss');
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
@@ -27,3 +27,18 @@ fetchISSFlyOverTimes(
     return console.log("It worked! Returned flyover times:",data);
   }
 );
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
--- iss.js
+++ iss.js
@@ -54,4 +54,26 @@ const fetchISSFlyOverTimes = function (coords, callback) {
     }
   );
 };
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+
+    }
 module.exports = { fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes };
